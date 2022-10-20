1d460791adf92c2a466468be065e7475ae9ed28e
Rodrigo <rodmorlei@gmail.com>
2022-10-19 21:16:58 -0300

    Telas para exibir a imagem

---
+++ TrabalhoPraticoModulo1/app/src/main/java/com/example/trabalhopraticomodulo1/GiftShow.kt
@@ -0,0 +1,11 @@
+package com.example.trabalhopraticomodulo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GiftShow : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gift_show)
+    }
+}
--- TrabalhoPraticoModulo1/app/src/main/java/com/example/trabalhopraticomodulo1/MainActivity.kt
+++ TrabalhoPraticoModulo1/app/src/main/java/com/example/trabalhopraticomodulo1/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.trabalhopraticomodulo1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.btnOpenPresent)
+        button.setOnClickListener {
+            val intent = Intent(this, GiftShow::class.java)
+            startActivity(intent)
+        }
+
     }
 }
