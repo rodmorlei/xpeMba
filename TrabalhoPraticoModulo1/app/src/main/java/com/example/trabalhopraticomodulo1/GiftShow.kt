13cfa485fc5bdfbb3d4dc0f647b38113ba33b1d7
Rodrigo <rodmorlei@gmail.com>
2022-10-25 18:05:29 -0300

    Adicionar o log no ciclo de vida

--- TrabalhoPraticoModulo1/app/src/main/java/com/example/trabalhopraticomodulo1/GiftShow.kt
+++ TrabalhoPraticoModulo1/app/src/main/java/com/example/trabalhopraticomodulo1/GiftShow.kt
@@ -2,10 +2,12 @@ package com.example.trabalhopraticomodulo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class GiftShow : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gift_show)
+        Log.i("Log 2", "Tela de presente apresentada")
     }
 }
