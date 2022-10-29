d6eb91057f3dde60e0acef6aab5fcaed8ddfe84c
Rodrigo <rodmorlei@gmail.com>
2022-10-29 11:37:44 -0300

    Ajustes iniciais

---
+++ DesafioModulo1/app/src/main/java/com/example/desafiomodulo1/ui/incluiritem/IncluirItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.desafiomodulo1.ui.incluiritem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.desafiomodulo1.R
+
+class IncluirItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_incluir_item)
+    }
+}
