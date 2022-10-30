8592c3a1e3b47694d69b0f86c8142f4877e9377d
Rodrigo <rodmorlei@gmail.com>
2022-10-29 22:51:59 -0300

    Preparar o viewmodel para mostrar a lista

---
+++ DesafioModulo1/app/src/main/java/com/example/desafiomodulo1/ui/main/MainViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.desafiomodulo1.ui.main
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.desafiomodulo1.data.Item
+import com.example.desafiomodulo1.data.repository.MemoryRepository
+
+class MainViewModel: ViewModel() {
+    private var memoryRepository: MemoryRepository = MemoryRepository(mutableListOf())
+    private val _listadeItens = MutableLiveData<List<Item>>()
+
+    fun iniciarDados(){
+        _listadeItens.value = memoryRepository.retornarLista()
+    }
+
+    fun salvarItem(item: Item) {
+        Log.i("ItemNOVO", "Item recebido: $item")
+
+        memoryRepository.incluir(item)
+        atualizarListaItem()
+
+    }
+
+    fun limparListaItem() {
+        memoryRepository.limparLista()
+        atualizarListaItem()
+    }
+
+    private fun atualizarListaItem() {
+        _listadeItens.value = memoryRepository.retornarLista()
+    }
+
+}
