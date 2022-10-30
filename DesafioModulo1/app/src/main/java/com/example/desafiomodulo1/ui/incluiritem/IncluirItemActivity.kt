90e13a3fb9fb878c1d3cef69b459ecd1c78ac3fb
Rodrigo <rodmorlei@gmail.com>
2022-10-29 21:26:40 -0300

    Adicionar alguns métodos

--- DesafioModulo1/app/src/main/java/com/example/desafiomodulo1/ui/incluiritem/IncluirItemActivity.kt
+++ DesafioModulo1/app/src/main/java/com/example/desafiomodulo1/ui/incluiritem/IncluirItemActivity.kt
@@ -2,11 +2,55 @@ package com.example.desafiomodulo1.ui.incluiritem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.desafiomodulo1.R
+import com.example.desafiomodulo1.databinding.ActivityIncluirItemBinding
 
 class IncluirItemActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIncluirItemBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_incluir_item)
+        binding = ActivityIncluirItemBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        configurarListeners()
+    }
+
+    private fun configurarListeners() {
+        configurarListenerBtnCancelar()
+        configurarListenerBtnIncluir()
+    }
+
+    private fun configurarListenerBtnIncluir() {
+        binding.btnIncluir.setOnClickListener {
+            incluirItem()
+        }
+    }
+
+    private fun incluirItem() {
+        val descricao = binding.descricaoItemET.text.toString()
+        val quantidade = binding.quantidadeItemET.text.toString()
+
+        if (descricao.trim().isEmpty()) {
+            binding.descricaoItemTIL.error = getString(R.string.err_sem_descricao)
+        } else {
+            binding.descricaoItemTIL.error = null;
+        }
+
+        if (quantidade.trim().isEmpty()) {
+            binding.quantidadeItemTIL.error = getString(R.string.err_sem_quantidade)
+        } else {
+            binding.quantidadeItemTIL.error = null
+        }
+
+        if (descricao.trim().isNotEmpty() && quantidade.trim().isNotEmpty()){
+            Toast.makeText(applicationContext, "Item $descricao incluído com sucesso", Toast.LENGTH_LONG).show()
+            finish()
+        }
+
+    }
+
+    private fun configurarListenerBtnCancelar() {
+        binding.btnCancelar.setOnClickListener { finish() }
     }
 }
--- DesafioModulo1/app/src/main/java/com/example/desafiomodulo1/ui/main/MainActivity.kt
+++ DesafioModulo1/app/src/main/java/com/example/desafiomodulo1/ui/main/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.example.desafiomodulo1.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.desafiomodulo1.databinding.ActivityMainBinding
+import com.example.desafiomodulo1.ui.incluiritem.IncluirItemActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        configurarListeners()
+
+    }
+
+    private fun configurarListeners() {
+        configurarFabListener()
+    }
+
+    private fun configurarFabListener() {
+        binding.fabAddNovoItem.setOnClickListener {
+            val intent = Intent(this, IncluirItemActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
