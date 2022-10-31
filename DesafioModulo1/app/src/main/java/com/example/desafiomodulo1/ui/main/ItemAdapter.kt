842f6af8a6f9be0aacd27cfc589ca1ab82d17fd9
Rodrigo <rodmorlei@gmail.com>
2022-10-31 09:47:53 -0300

    Criar o adpter da lista

---
+++ DesafioModulo1/app/src/main/java/com/example/desafiomodulo1/ui/main/ItemAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.desafiomodulo1.ui.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.desafiomodulo1.data.Item
+import com.example.desafiomodulo1.databinding.ListItemItemBinding
+
+class ItemAdapter(private val listaItem: List<Item>): RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+
+    private lateinit var  binding: ListItemItemBinding
+
+    inner class ViewHolder(view: View):RecyclerView.ViewHolder(view) {
+
+        fun bind(item: Item) {
+            binding.apply {
+                tvDescricao.text = item.descricao
+                tvQuantidade.text = item.quantidade
+            }
+        }
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        binding = ListItemItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(item= listaItem[position])
+    }
+
+    override fun getItemCount(): Int = listaItem.size
+
+}
