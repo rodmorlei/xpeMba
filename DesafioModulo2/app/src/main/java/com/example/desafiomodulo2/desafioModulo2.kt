757f105ba7ed2633d4d21c68c807b26dafcbd4a8
Rodrigo <rodmorlei@gmail.com>
2022-11-09 19:46:01 -0300

    Implementar os 3 métodos, inclusive o do quadrado do numero como pede o exercicio.

--- DesafioModulo2/app/src/main/java/com/example/desafiomodulo2/desafioModulo2.kt
+++ DesafioModulo2/app/src/main/java/com/example/desafiomodulo2/desafioModulo2.kt
@@ -1,5 +1,25 @@
 package com.example.desafiomodulo2
 
+import kotlin.math.sqrt
+
 fun main() {
     println("Hello World")
+    val x: Int = sum(2, 3)
+    println("Valor Somado: $x" )
+    val y: Int = valorQuadrado(9)
+    println("Valor Quadrado: $y" )
+    val z: Float = raizQuadrada(y.toFloat())
+    println("Valor Quadrado: $z" )
+}
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun valorQuadrado(a: Int): Int {
+    return a * a
+}
+
+fun raizQuadrada(a: Float): Float {
+    return sqrt(a)
 }
